0bd9aff7fff4c7cd48f1e1b5dd2417b249883492
Rock Chin <1010553892@qq.com>
2023-02-15 15:34:19 +0800

    feat: 基本配置

--- main.py
+++ main.py
@@ -1,9 +1,32 @@
+import traceback
+
 from pkg.plugin.models import *
 from pkg.plugin.host import EventContext, PluginHost
+
+import os
 
 """
 接入ChatGPT的逆向库
 """
+
+
+def check_config():
+    this_file = __file__
+
+    template_file = os.path.join(os.path.dirname(this_file), "revcfg-template.py")
+
+    # 检查revlib.py是否存在
+    if not os.path.exists("revcfg.py"):
+        # 不存在则使用本模块同目录的revcfg-template.py复制创建
+        with open(template_file, "r", encoding="utf-8") as f:
+            template = f.read()
+
+        with open("revcfg.py", "w", encoding="utf-8") as f:
+            f.write(template)
+
+        return False
+
+    return True
 
 
 # 注册插件
@@ -11,19 +34,47 @@ from pkg.plugin.host import EventContext, PluginHost
 class HelloPlugin(Plugin):
 
     # 插件加载时触发
-    # plugin_host (pkg.plugin.host.PluginHost) 提供了与主程序交互的一些方法，详细请查看其源码
     def __init__(self, plugin_host: PluginHost):
-        pass
+        if not check_config():
+            logging.error("[rev] 已生成配置文件(revcfg.py)，请按照其中注释填写配置文件后重启程序")
+            return
 
-    # 当收到个人消息时触发
-    @on(PersonNormalMessageReceived)
-    def person_normal_message_received(self, event: EventContext, **kwargs):
-        pass
+        # 当收到个人消息时触发
+        @on(PersonNormalMessageReceived)
+        def person_normal_message_received(inst, event: EventContext, **kwargs):
+            pass
 
-    # 当收到群消息时触发
-    @on(GroupNormalMessageReceived)
-    def group_normal_message_received(self, event: EventContext, **kwargs):
-        pass
+        # 当收到群消息时触发
+        @on(GroupNormalMessageReceived)
+        def group_normal_message_received(inst, event: EventContext, **kwargs):
+            pass
+
+        import revcfg
+
+        from revChatGPT.V1 import Chatbot
+
+        try:
+
+            self.chatbot = Chatbot(
+                config=revcfg.openai_account
+            )
+
+            self.debug("你好！")
+        except:
+            # 输出完整的错误信息
+            logging.error("[rev] 逆向库初始化失败，请检查配置文件(revcfg.py)是否正确")
+            logging.error("[rev] " + traceback.format_exc())
+
+    def debug(self, msg):
+        logging.debug("[rev] " + msg)
+        for data in self.chatbot.ask(
+            msg,
+            conversation_id=self.chatbot.config.get('conversation'),
+            parent_id=self.chatbot.config.get('parent_id'),
+        ):
+            print(data['message'], end="", flush=True)
+
+        print()
 
     # 插件卸载时触发
     def __del__(self):
---
+++ revcfg-template.py
@@ -0,0 +1,5 @@
+# OpenAI账户信息
+openai_account = {
+    "email": "your email",  # 账户邮箱
+    "password": "your password"  # 账户密码
+}
