7cf14e7066e78e36a199b71e4cdd97123b28d7c3
Rock Chin <1010553892@qq.com>
2023-02-19 12:03:25 +0800

    chore: 删除无用的文件

--- pkg/models/extcomponent.py
+++
@@ -1,9 +0,0 @@
-from mirai.models.message import MessageComponent
-
-class ForwardMessage(MessageComponent):
-
-    type: str = "Forward"
-    nodeList: list
-
-    def __str__(self):
-        return "[转发消息]"
