29bf7b5fac11fc336812ae0dc7a733c245a1d2c3
Rock Chin <1010553892@qq.com>
2023-03-15 11:08:25 +0800

    feat: 添加文件

---
+++ pkg/process/impls/edgegpt.py
@@ -0,0 +1,17 @@
+"""接入acheone08/EdgeGPT
+"""
+from plugins.revLibs.pkg.models.interface import RevLibInterface
+
+
+class EdgeGPTImpl(RevLibInterface):
+    """使用acheong08/EdgeGPT接入new bing
+    """
+
+    @staticmethod
+    def create_instance():
+        pass
+
+    def __init__(self):
+        pass
+
+
