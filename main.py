883acb1bb4c95ebf02a57f91bca49ecc1fa48d55
RockChinQ <1010553892@qq.com>
2023-12-10 23:01:35 +0800

    Release v0.9.1

--- main.py
+++ main.py
@@ -34,7 +34,7 @@ def check_config():
 
 
 # 注册插件
-@register(name="revLibs", description="接入acheong08/ChatGPT、Claude、Bard等逆向库", version="0.9.0", author="RockChinQ")
+@register(name="revLibs", description="接入acheong08/ChatGPT、Claude、Bard等逆向库", version="0.9.1", author="RockChinQ")
 class RevLibsPlugin(Plugin):
 
     chatbot: Chatbot = None
